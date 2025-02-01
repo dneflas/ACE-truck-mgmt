298018d041fff0f6530149618e0017bf5a4ebd6d
Debbie Neflas <debbieneflas@gmail.com>
2025-02-01 15:38:40 -0800

    add in placeholder orders and trucks pages

--- src/app.jsx
+++ src/app.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Orders from "./pages/Orders";
+import Trucks from "./pages/Trucks";
 import ErrorPage from "./pages/ErrorPage";
 import Nav from "./components/Nav";
 
@@ -12,6 +14,8 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="/trucks" element={<Trucks />} />
         <Route path="*" element={<ErrorPage />} />
       </Routes>
     </>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,7 +4,8 @@ import { Link } from "react-router-dom";
 const Nav = () => {
   return (
     <nav>
-      <Link to="/">Home</Link> | <Link to="/about">About</Link>;
+      <Link to="/">Home</Link> | <Link to="/about">About</Link> |{" "}
+      <Link to="/orders">Orders</Link> | <Link to="/trucks">Trucks</Link>;
     </nav>
   );
 };
